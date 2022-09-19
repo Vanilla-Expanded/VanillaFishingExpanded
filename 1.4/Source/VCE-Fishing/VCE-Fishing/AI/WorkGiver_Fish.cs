8a12e37377603f0b6036ea47368d63e3f0bd0afa
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-09-19 13:12:29 +0200

    v3.0

---
+++ 1.4/Source/VCE-Fishing/VCE-Fishing/AI/WorkGiver_Fish.cs
@@ -0,0 +1,95 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Verse;
+using Verse.AI;
+
+namespace VCE_Fishing
+{
+    class WorkGiver_Fish : WorkGiver_Scanner
+    {
+
+        public override PathEndMode PathEndMode
+        {
+            get
+            {
+                return PathEndMode.Touch;
+            }
+
+        }
+
+
+        [DebuggerHidden]
+        public override IEnumerable<IntVec3> PotentialWorkCellsGlobal(Pawn pawn)
+        {
+            Danger maxDanger = pawn.NormalMaxDanger();
+            
+           
+            List<Zone> zonesList = pawn.Map.zoneManager.AllZones;
+            for (int j = 0; j < zonesList.Count; j++)
+            {
+                Zone_Fishing fishingZone = zonesList[j] as Zone_Fishing;
+                if (fishingZone != null)
+                {
+                    if (fishingZone.cells.Count == 0)
+                    {
+                        Log.ErrorOnce("Fishing zone has 0 cells (this should never happen): " + fishingZone, -563487);
+                    }
+                    if (!fishingZone.someoneFishing) {
+                        if (fishingZone.allowFishing)
+                        {
+                            if (!fishingZone.isZonePolluted)
+                            {
+                                if (fishingZone.isZoneBigEnough)
+                                {
+                                    if (!fishingZone.isZoneEmpty)
+                                    {
+                                        if (!fishingZone.ContainsStaticFire)
+                                        {
+                                            if (pawn.CanReserveAndReach(fishingZone.Cells[0], PathEndMode.OnCell, maxDanger))
+                                            {
+                                                for (int k = 0; k < fishingZone.cells.Count; k++)
+                                                {
+                                                    yield return fishingZone.cells[k];
+                                                }
+
+                                            }
+                                        }
+
+
+                                    }
+                                }
+
+
+
+                            }
+
+                        }
+
+                    }
+                    
+                    
+                }
+            }
+           
+        }
+
+        public override Job JobOnCell(Pawn pawn, IntVec3 c, bool forced = false)
+        {
+           
+            LocalTargetInfo target = c;
+           
+            if (!pawn.CanReserve(target, 1, -1, null, false))
+            {
+                return null;
+            }
+            if (ModsConfig.IdeologyActive && DefDatabase<HistoryEventDef>.GetNamedSilentFail("VME_Fishing")!=null && !new HistoryEvent(DefDatabase<HistoryEventDef>.GetNamedSilentFail("VME_Fishing"), pawn.Named(HistoryEventArgsNames.Doer)).Notify_PawnAboutToDo_Job())
+            {
+                return null;
+            }
+            Job job = new Job(DefDatabase<JobDef>.GetNamed("VCEF_FishJob"),c);
+            return job;
+        }
+
+    }
+}
---
+++ 1.4/Source/VCE-Fishing/VCE-Fishing/MapComponents/FishingMapComponent.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+using UnityEngine;
+using RimWorld.Planet;
+
+namespace VCE_Fishing
+{
+    public class FishingMapComponent : MapComponent
+    {
+
+        private int fishTickProgress = 0;
+        private int fishTickMax = 128;
+
+        public FishingMapComponent(Map map) : base(map)
+        {
+            
+        }
+        public override void MapComponentTick()
+        {
+            base.MapComponentTick();
+
+            if (fishTickProgress>= fishTickMax) {
+                foreach (Zone zone in this.map.zoneManager.AllZones.Where(zone => zone.GetType() == typeof(Zone_Fishing)))
+                {
+                    
+                    Zone_Fishing zoneFishing = zone as Zone_Fishing;
+
+                    zoneFishing.isZonePolluted = false;
+                    if (ModsConfig.BiotechActive)
+                    {
+
+                        foreach (IntVec3 cell in zoneFishing.cells)
+                        {
+
+                            if (cell.IsPolluted(zoneFishing.Map))
+                            {
+
+                                zoneFishing.isZonePolluted = true;
+                            }
+
+                        }
+                    }
+
+
+                    if (zoneFishing.cells.Count < Options.VCE_Fishing_Settings.VCEF_minimumZoneSize)
+                    {
+                        zoneFishing.isZoneBigEnough = false;
+                    } else zoneFishing.isZoneBigEnough = true;
+
+                    int index = 0;
+                    while (index < zoneFishing.cells.Count)
+                    {
+                       if (zoneFishing.cells[index].GetTerrain(this.map).defName== "WaterOceanDeep"|| zoneFishing.cells[index].GetTerrain(this.map).defName == "WaterOceanShallow")
+                       {
+                           
+                            zoneFishing.isOcean = true;
+                            zoneFishing.initialSetZoneFishList();
+                            break;
+                       }
+                       index++;
+                       zoneFishing.isOcean = false;
+                    }
+                }
+                fishTickProgress = 0;
+            }
+            fishTickProgress++;
+
+
+
+        }
+    }
+}
