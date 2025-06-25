2cb12cf036e5d665d9c3c021c8b48f426f18711c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-25 19:38:37 +0200

    v5.0

---
+++ 1.6Odyssey/Source/VCE-Fishing/VCE-Fishing/DefOfs/InternalDefOf.cs
@@ -0,0 +1,26 @@
+﻿
+using RimWorld;
+using Verse;
+
+
+namespace VCE_Fishing
+{
+	[DefOf]
+	public static class InternalDefOf
+	{
+	
+
+		public static TraitDef VCEF_Fisherman;
+
+		public static ThoughtDef VCEF_FishingThought;
+
+		public static HediffDef VCEF_CaughtSpecialHediff;
+
+        public static StatDef VCEF_FishingLuckOffset;
+
+        static InternalDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+		}
+	}
+}
